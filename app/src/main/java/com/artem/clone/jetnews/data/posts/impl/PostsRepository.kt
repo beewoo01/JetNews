92658bf15985c6d05888fc92384027a01efe79fe
hyunwoo <beewoo01@naver.com>
2025-07-11 22:34:33 +0900

    build post repository

--- app/src/main/java/com/artem/clone/jetnews/data/posts/impl/PostsRepository.kt
+++ app/src/main/java/com/artem/clone/jetnews/data/posts/impl/PostsRepository.kt
@@ -1,5 +1,19 @@
 package com.artem.clone.jetnews.data.posts.impl
 
+import com.artem.clone.jetnews.model.Post
+import com.artem.clone.jetnews.model.PostsFeed
+import kotlinx.coroutines.flow.Flow
+
 interface PostsRepository {
+
     suspend fun getPost(postId: String?) : Result<Post>
+
+    suspend fun getPostsFeed() : Result<PostsFeed>
+
+    fun observeFavorites() : Flow<Set<String>>
+    //fun observeFavorites() : Flow<Set<String>>
+    fun observePostsFeed() : Flow<String?>
+
+    suspend fun toggleFavorite(postId: String)
+
 }
